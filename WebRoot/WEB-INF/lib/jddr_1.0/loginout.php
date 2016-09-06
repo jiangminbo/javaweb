41ed79c551c36a4d434175e0ecd1230caa74575d
jiangminbo <jiangminbo@qq.com>
2016-09-06 16:58:24 +0800

    llllll

--- WebRoot/WEB-INF/lib/jddr_1.0/loginout.php
+++
@@ -1,6 +0,0 @@
-<?php
-//注销登录
-session_start();
-$_SESSION['username']="";
-echo "<script language='javascript'>alert('注销登录成功！');location='login.php';</script>";
-?>
--- WebRoot/WEB-INF/lib/jddr_1.0/readme.txt
+++
@@ -1,8 +0,0 @@
-将database.sql导入到mysql
-修改conn.php里的链接配置
-默认用户名密码都是admin
-暂时没有密码修改功能，请到数据库里修改
-密码采用MD5加密
-
-设计制作：杨云峰
-Email：yunfengyang@foxmail.com
